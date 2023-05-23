c26993a64f13a1161b240b65e4ad3cd6c0cfe3bb
Jose A. Fernandez <fernandezja@gmail.com>
2023-05-23 16:40:40 -0300

    [DataAccessRepoDb] ExecuteScalar

--- src/DataAccessRepoDb/DataAccessRepoDb/Program.cs
+++ src/DataAccessRepoDb/DataAccessRepoDb/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Data.SqlClient;
 using RepoDb;
 using System;
+using System.Diagnostics.Metrics;
 
 Console.WriteLine("Data Access with RepoDB!");
 
@@ -31,8 +32,32 @@ IEnumerable<Movie> moviesStarwars;
 using (var connection = new SqlConnection(connectionString))
 {
     moviesStarwars = connection.Query<Movie>(m => m.Name.Contains("Starwars"));
+
 }
 
 Console.WriteLine($"Movies Starwars count: {moviesStarwars.Count()}");
+
+
+long count2;
+using (var connection = new SqlConnection(connectionString))
+{
+    var sql = "SELECT COUNT(*) FROM [dbo].[Movie];";
+    count2 = connection.ExecuteScalar<long>(sql);
+}
+
+
+Console.WriteLine($"Movies  count 2: {count2}");
+
+
+//1: Insertar 10mil peliculas (o entidades de su negocio)
+//Ejemplo "pelicula 123456789"
+
+//Actualizar solo peliculas que terminan en "666" con "(XX)"
+//"Pelicula ...666 (X)"
+//Ejemplo 
+//  ANTES: Pelicula 666  >>  Pelicula 666 (X)
+//  ANTES: Pelicula 1666  >> Pelicula 1666 (X)
+//  ANTES: Pelicula 2666  >> Pelicula 2666 (X)
+
 
 Console.ReadKey();
