5c099adaefc76dd2d85e2e74c4ea3a0fe6ef0328
Jose A. Fernandez <fernandezja@gmail.com>
2023-05-23 16:18:41 -0300

    [DataAccessRepoDb] Query/QueryAll

---
+++ src/DataAccessRepoDb/DataAccessRepoDb.Entities/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace DataAccessRepoDb.Entities
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
---
+++ src/DataAccessRepoDb/DataAccessRepoDb/Program.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessRepoDb.Entities;
+using Microsoft.Data.SqlClient;
+using RepoDb;
+using System;
+
+Console.WriteLine("Data Access with RepoDB!");
+
+string connectionString =
+            "Persist Security Info=True;Initial Catalog=Movies;Data Source=.; Integrated Security=True;TrustServerCertificate=True;";
+
+GlobalConfiguration
+    .Setup()
+    .UseSqlServer();
+
+//Movies
+IEnumerable<Movie> movies;
+
+//ORM
+using (var connection = new SqlConnection(connectionString))
+{
+    movies = connection.QueryAll<Movie>();
+}
+
+var count = movies.Count();
+
+Console.WriteLine($"Movies count: {count}");
+
+
+//Query
+IEnumerable<Movie> moviesStarwars;
+using (var connection = new SqlConnection(connectionString))
+{
+    moviesStarwars = connection.Query<Movie>(m => m.Name.Contains("Starwars"));
+}
+
+Console.WriteLine($"Movies Starwars count: {moviesStarwars.Count()}");
+
+Console.ReadKey();
